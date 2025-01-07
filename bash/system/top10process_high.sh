45b24209d1d46bc032490f29b27b4edf8997b4d8
Juan Pablo Perez <perez.juan@supercanal.tv>
2025-01-07 08:56:28 -0300

    Actualizacion desde PowerShell

--- bash/system/top10process_high.sh
+++ bash/system/top10process_high.sh
@@ -4,7 +4,7 @@ clear
 echo "Proocesos de mayor consumo de CPU"
 echo " "
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
-echo " "
-echo "Segundo listado (incluyen path)..."
-echo " "
-ps -eo pcpu,pid,user,args | tail -n +2 | sort -k1 -r -n | head -10
+#echo " "
+#echo "Segundo listado (incluyen path)..."
+#echo " "
+#ps -eo pcpu,pid,user,args | tail -n +2 | sort -k1 -r -n | head -10
